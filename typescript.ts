836f9160acb03fd6ef3997d89f3312a3471faff5
q.chen <q7but@qq.com>
2020-05-27 17:46:11 +0800

    update

--- typescript.ts
+++ typescript.ts
@@ -1,49 +1,11 @@
-// class Student {
-//   fullName: string;
-//   constructor(public firstName, public middleInitial, public lastName) {
-//     this.fullName = firstName + " " + middleInitial + " " + lastName;
-//     console.log(this.fullName)
-//   }
-// }
-
-// interface Person {
-//   firstName: string;
-//   lastName: string;
-// }
-
-// function greeter(person : Person) {
-//   console.log(person) ;
-
-// }
-
-// let user = new Student("Jane", "M.", "User");
-
-// greeter(user);
-
-
-let passcode = "secret passcode";
-
-class Employee {
-    private _fullName: string;
-
-    get fullName(): string {
-        console.log('get==>')
-        return this._fullName;
-    }
-
-    set fullName(newName: string) {
-      console.log('set==>',newName)
-        if (passcode && passcode == "secret passcode") {
-            this._fullName = newName;
-        }
-        else {
-            console.log("Error: Unauthorized update of employee!");
-        }
-    }
+class GenericNumber<T> {
+    zeroValue: T;
+    add: (x: T, y: T) => T;
 }
 
-let employee = new Employee();
-employee.fullName = "Bob Smith";
-if (employee.fullName) {
-    console.log(employee.fullName);
-}
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
+
+
+console.log(myGenericNumber)
