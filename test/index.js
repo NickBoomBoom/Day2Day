4f25227980606a9e7ca140886d7d44a8d5ab9461
q.chen <q.chen.self@outlook.com>
2024-11-04 13:08:35 +0800

    update

---
+++ test/index.js
@@ -0,0 +1,21 @@
+const { io }  = require('socket.io-client')
+
+// const socket = io('http://localhost:3000', {
+// const socket = io('https://admin-test.betterparenting.online', {
+// const socket = io('https://admin-api2.betterparenting.online', {
+  query: {
+    userId: 1,
+    tag: 'admin'
+  }
+})
+socket.on('connect', () => {
+  console.log('连接成功')
+})
+
+socket.on('connect_error', (error ) => {
+  console.error('连接报错', error.message)
+})
+
+socket.on('disconnect', () => {
+  console.log('断开')
+})
