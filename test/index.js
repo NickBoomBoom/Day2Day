66cb59715e6f7f6a674fc0caeef36dc8d93a7303
q.chen <q.chen.self@outlook.com>
2024-11-06 15:18:23 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 const { io }  = require('socket.io-client')
 
 // const socket = io('http://localhost:3000', {
-// const socket = io('https://admin-test.betterparenting.online', {
-// const socket = io('https://admin-api2.betterparenting.online', {
+const socket = io('https://admin-test.betterparenting.online', {
+// const socket = io('https://api2.betterparenting.online/admin2', {
   query: {
     userId: 1,
     tag: 'admin'
